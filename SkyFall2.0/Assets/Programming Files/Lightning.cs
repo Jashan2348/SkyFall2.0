a5c240031588cb9b8b32332bc3218921734cc71d
Arash2348 <46118628+Arash2348@users.noreply.github.com>
2020-07-09 15:59:50 -0700

    Arash update
    cloud changes

---
+++ SkyFall2.0/Assets/Programming Files/Lightning.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour
+{
+   
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+}
---
+++ SkyFall2.0/Assets/Programming Files/enemyAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAI : MonoBehaviour
+{
+    public Transform player; 
+    public Transform cloud;
+
+    public Transform firePoint;
+
+    public float distance;
+      
+
+    void Start(){
+     distance = Vector3.Distance(player.transform.position, cloud.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        findTarget();
+    }
+
+    public void findTarget(){
+        float targetRange = 50f;
+        if(distance < targetRange){
+            Shoot();
+        }
+    }
+
+    public void Shoot(){
+        
+    }
+}
+
+
